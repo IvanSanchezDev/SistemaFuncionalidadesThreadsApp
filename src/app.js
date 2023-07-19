43c3a1123a5170423163ac692c78ea18d1e6b9f1
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-07-19 09:27:19 -0500

    feat: :sparkles: Se agrego  el middleware del ruteo para las funciones de usuarios y middleware para permitir json

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv'
+import userRouter from './routes/user.routes.js';
 
 dotenv.config()
 
 const app=express();
 
-
+app.use(express.json())
+app.use('/user', userRouter)
 
 
 app.listen(5001, ()=>{
