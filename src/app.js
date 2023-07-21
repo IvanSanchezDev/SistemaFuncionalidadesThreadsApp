0df337e0182eea08ebe94e6c7e9ca861b4fbc21b
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-07-20 22:58:53 -0500

    feat: :sparkles: permitir usar las cookies y el bodyParser

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import userRouter from './routes/user.routes.js';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 
 dotenv.config()
 
 const app=express();
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json())
+app.use(cookieParser());
 app.use('/user', userRouter)
 
 
