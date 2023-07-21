9fe5b0d5ed654a965ea5dc4e8611ca029a1b76be
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-07-20 23:00:14 -0500

    refactor: :recycle: Modificacion de la funcion para verificar el token y extraer la data

--- src/Middlewares/jwt.js
+++ src/Middlewares/jwt.js
@@ -4,16 +4,12 @@ export function generateAccessToken(user) {
     return jwt.sign(user, process.env.JWT_PRIMARY_KEY, { expiresIn: "5m" });
   }
   
-export  function verifyToken() {
-    const { authorization } = req.cookies;
-    if (!authorization) res.send("Acces Denied");
-  
-    jwt.verify(authorization, process.env.JWT_PRIMARY_KEY, (err, decode) => {
-      if (err) {
-        res.send("Acces Denied o expired");
-      } else {
-        req.user = decode;
-      }
-    });
+export  function verifyToken(authorization) {
+  try {
+    const decode = jwt.verify(authorization, process.env.JWT_PRIMARY_KEY);
+    return decode;
+  } catch (err) {
+    return "Acces Denied o expired";
+  }
   }
 
