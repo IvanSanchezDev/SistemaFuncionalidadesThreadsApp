bc5084651711988f12b81104eb0e0b21715f2fee
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-07-22 18:44:49 -0500

    feat: :sparkles: Implementacion añadir comentarios en las publicaciones

---
+++ src/routes/comment.routes.js
@@ -0,0 +1,29 @@
+import { Router } from "express";
+import getConnection from "../db/database.js";
+import { verifyToken } from "../Middlewares/jwt.js";
+
+const commentRouter = Router();
+
+commentRouter.post("/addComment", verifyToken, async (req, res) => {
+  try {
+    const { user_id } = req.user;
+    const { comment, post_id } = req.body;
+    const con = await getConnection();
+    const [result] = await con.execute(
+      "INSERT INTO comments (comment, post_id, user_id) VALUES (?,?,?)",
+      [comment, post_id, user_id]
+    );
+
+    if (result.affectedRows != 1) {
+      return res
+        .status(500)
+        .send("no se pudo subir hacer el comentario, intentelo de nuevo");
+    }
+
+    return res.status(200).json({ message: `Comentario Subido Correctamente en el post ${post_id}` });
+  } catch (error) {
+    res.status(500).json({ message: "error server" });
+  }
+});
+
+export default commentRouter;
