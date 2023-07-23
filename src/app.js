d4444e062e514bc0bc5d43abc2e46e9934f01f4a
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-07-22 19:11:37 -0500

    feat: :sparkles: Implementacion de los middlewares de ruteo de los comentarios y likes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import userRouter from './routes/user.routes.js';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import postRouter from './routes/post.routes.js';
+import commentRouter from './routes/comment.routes.js';
+import likeRouter from './routes/like.routes.js';
 
 dotenv.config()
 
@@ -13,6 +15,8 @@ app.use(express.json())
 app.use(cookieParser());
 app.use('/user', userRouter)
 app.use('/post', postRouter)
+app.use('/comment', commentRouter)
+app.use('/like', likeRouter)
 
 
 app.listen(5001, ()=>{
