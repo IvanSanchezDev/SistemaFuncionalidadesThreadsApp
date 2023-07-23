434a26151c2e1f896268ff484d1b9161e916b50a
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-07-23 16:59:25 -0500

    refactor: :recycle: Mas campos en el registro de usuarios  y eliminacion de confirmacion de la cuenta mediante correo

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import getConnection from "../db/database.js";
 import dotenv from "dotenv";
 import { generateAccessToken, verifyToken } from "../Middlewares/jwt.js";
-import { encrypt, compare} from '../Helpers/handleBcrypt.js'
+import { encrypt, compare } from "../Helpers/handleBcrypt.js";
 
 import { envioCorreo } from "../Helpers/sendEmail.js";
 
@@ -10,8 +10,8 @@ dotenv.config();
 
 const userRouter = Router();
 
-//PRUEBA 
-userRouter.get("/", (req, res)=>{
+//PRUEBA
+userRouter.get("/", (req, res) => {
   res.send(`
     <html>
       <head>
@@ -25,41 +25,41 @@ userRouter.get("/", (req, res)=>{
         </form>
       </body>
     </html>
-  `)
-})
+  `);
+});
 
 userRouter.post("/auth", async (req, res) => {
   try {
     const con = await getConnection();
-    
-    const { email, password } = req.body;
-    const user={email}
-    const passwordHash=await encrypt(password)
+
+    const { email, password, username, detalles, telefono, codigoPostal } =
+      req.body;
+
+    const passwordHash = await encrypt(password);
 
     const [existsUser] = await con.execute(
-      "SELECT * FROM users WHERE email=?",
-      [email]
+      "SELECT * FROM users WHERE email=? or username=?",
+      [email, username]
     );
 
     if (existsUser.length > 0) {
-      return res.status(409).send("El nombre de usuario ya está en uso.");
+      return res.status(409).send("El correo o username ya está en uso.");
     }
 
     const [result] = await con.execute(
-      "INSERT INTO users (email, password) VALUES (?,?)",
-      [email, passwordHash]
+      "INSERT INTO users (email, password, username, detalles, telefono, codigoPostal) VALUES (?,?,?,?,?,?)",
+      [email, passwordHash, username, detalles, telefono, codigoPostal]
     );
 
     if (result.affectedRows !== 1) {
       return res.status(500).send("No se pudo crear el usuario.");
     }
 
-    const accessToken = generateAccessToken(user);
-    envioCorreo(email);
+    const accessToken = generateAccessToken({email});
+    //envioCorreo(email);
     res.cookie("token", accessToken, { httpOnly: true });
-    res.status(200).json({
-      message:
-        "Usuario registrado correctamente. Revise su correo electrónico para confirmar su cuenta.",
+    res.status(201).json({
+      message: "Usuario registrado correctamente.",
     });
   } catch (error) {
     res.status(401).send(error.message);
@@ -68,9 +68,7 @@ userRouter.post("/auth", async (req, res) => {
 
 userRouter.get("/confirmacion", verifyToken, async (req, res) => {
   try {
- 
-    const {email} = req.user;
-  
+    const { email } = req.user;
 
     const con = await getConnection();
     const [result] = await con.execute(
@@ -97,32 +95,28 @@ userRouter.post("/login", async (req, res) => {
     const { email, password } = req.body;
     const con = await getConnection();
 
-    
     const [rows] = await con.execute(
-      "SELECT user_id, email, password, verifyUser FROM users WHERE email=?",
+      "SELECT user_id, email, password FROM users WHERE email=?",
       [email]
     );
 
-
     if (rows.length >= 1) {
       //const { user_id, password, verifyUser } = rows[0];
-      const user=rows[0];
- 
-      if (!user.verifyUser) {
+      const user = rows[0];
+
+      /* if (!user.verifyUser) {
         return res
           .status(401)
           .send("Usuario no verificado. Por favor, verifica tu cuenta.");
-      }
+      }*/
 
-      const checkPassword=await compare(password, user.password)
+      const checkPassword = await compare(password, user.password);
 
       if (!checkPassword) {
-        return res
-        .status(401)
-        .send("Password Incorrecta");
+        return res.status(401).send("Password Incorrecta");
       }
 
-      const { user_id, email } = user
+      const { user_id, email } = user;
 
       const data = { user_id, email };
       const accessToken = generateAccessToken(data);
@@ -131,13 +125,11 @@ userRouter.post("/login", async (req, res) => {
 
       res.status(200).send("Ha ingresado Correctamente.");
     } else {
-      res.status(401).send("Usuario o contraseña incorrectos.");
+      res.status(400).send("Usuario o contraseña incorrectos.");
     }
   } catch (error) {
     res.status(500).send("Error en el servidor." + error.message);
   }
 });
-
-
 
 export default userRouter;
